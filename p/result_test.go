e82782720aa4a859b1d0e55d731dd68e16d7059f
Lukáš Simulík <lukas.simulik@gmail.com>
2020-04-28 19:40:37 +0200

    Add array tests
    Tests are using result comparision.

--- p/result_test.go
+++ p/result_test.go
@@ -11,60 +11,108 @@ import (
 func TestResult(t *testing.T) {
 	t.Helper()
 
-	src, err := ioutil.TempFile("", "php")
-	if err != nil {
-		t.Fatalf("opening temp file: %v", err)
-	}
-	defer src.Close()
+	for _, s := range []string{
+		// Sandbox
+		`<?php echo "a";`,
+		// examples/30.php
+		`<?php
 
-	s := `<?php echo "a";`
-	src.WriteString(s)
+		$a = ['a', 'b', 'c'];
 
-	cmd := exec.Command("php", "-f", src.Name())
-	var refOut bytes.Buffer
-	cmd.Stdout = &refOut
-	err = cmd.Run()
-	if err != nil {
-		t.Fatalf("executing php: %v", err)
-	}
+		foreach ($a as $k => $v) {
+			echo "$k: $v\n";
+		}
 
-	root, err := ioutil.TempDir("", "go-build")
-	if err != nil {
-		t.Fatalf("creating temp dir: %v", err)
-	}
+		foreach ([0, 1, 2, 3] as $k => $v) {
+			echo "$k: $v\n";
+		}
 
-	ref, err := ioutil.TempFile(root, "")
-	if err != nil {
-		t.Fatalf("opening temp file: %v", err)
-	}
-	defer ref.Close()
+		foreach ([0, 1, 2, 3] as $v) {
+			echo ": $v\n";
+		}
+		`,
+		// examples/31.php
+		`<?php
 
-	p := NewParser(NewNameTranslator(), NewFunctionTranslator())
-	res := p.Run(parsePHP([]byte(s)))
-	ref.WriteString(res.String())
+		$array = ['placeholder'];
+		$array['e'] = '5';
 
-	fileName := strings.TrimPrefix(ref.Name(), root+"/")
-	cmd = exec.Command("mv", fileName, "main.go")
-	cmd.Dir = root
-	err = cmd.Run()
-	if err != nil {
-		t.Fatalf("changing directory: %v", err)
-	}
+		if (isset($array['e'])) {
+			echo "\"e\": $array[e]\n";
+		} else {
+			echo "\"e\" is not set.\n";
+		}
 
-	cmd = exec.Command("go", "run", "main.go")
-	cmd.Dir = root
+		unset($array['e']);
 
-	var out bytes.Buffer
-	var errf bytes.Buffer
-	cmd.Stdout = &out
-	cmd.Stderr = &errf
-	err = cmd.Run()
-	if err != nil {
-		t.Error(errf.String())
-		t.Fatalf("executing go: %v", err)
-	}
+		if (isset($array['e'])) {
+			echo "\"e\": $array[e]\n";
+		} else {
+			echo "\"e\" is not set.\n";
+		}
 
-	if refOut.String() != out.String() {
-		t.Errorf("Expected:\n%s\nGot:\n%s\n", refOut.String(), out.String())
+		$array['e'] = 'f';
+		if (isset($array['e'])) {
+			echo "\"e\": $array[e]\n";
+		} else {
+			echo "\"e\" is not set.\n";
+		}
+		`,
+	} {
+		src, err := ioutil.TempFile("", "php")
+		if err != nil {
+			t.Fatalf("opening temp file: %v", err)
+		}
+		defer src.Close()
+
+		src.WriteString(s)
+
+		cmd := exec.Command("php", "-f", src.Name())
+		var refOut bytes.Buffer
+		cmd.Stdout = &refOut
+		err = cmd.Run()
+		if err != nil {
+			t.Fatalf("executing php: %v", err)
+		}
+
+		root, err := ioutil.TempDir("", "go-build")
+		if err != nil {
+			t.Fatalf("creating temp dir: %v", err)
+		}
+
+		ref, err := ioutil.TempFile(root, "")
+		if err != nil {
+			t.Fatalf("opening temp file: %v", err)
+		}
+		defer ref.Close()
+
+		p := NewParser(NewNameTranslator(), NewFunctionTranslator())
+		res := p.Run(parsePHP([]byte(s)))
+		ref.WriteString(res.String())
+
+		fileName := strings.TrimPrefix(ref.Name(), root+"/")
+		cmd = exec.Command("mv", fileName, "main.go")
+		cmd.Dir = root
+		err = cmd.Run()
+		if err != nil {
+			t.Fatalf("changing directory: %v", err)
+		}
+
+		cmd = exec.Command("go", "run", "main.go")
+		cmd.Dir = root
+
+		var out bytes.Buffer
+		var errf bytes.Buffer
+		cmd.Stdout = &out
+		cmd.Stderr = &errf
+		err = cmd.Run()
+		if err != nil {
+			t.Error(errf.String())
+			t.Fatalf("executing go: %v", err)
+		}
+
+		if refOut.String() != out.String() {
+			t.Errorf("Expected:\n%s\nGot:\n%s\n", refOut.String(), out.String())
+		}
 	}
 }
